1d054a5eb1dc379b2c71bf20d53889d83e2e369e
Sidinei Silva <sidinei.silva02@gmail.com>
2023-10-08 13:45:58 -0300

    feat: include app ValorantApp

---
+++ ValorantApp/app/src/main/java/com/sidinei/valorantapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.sidinei.valorantapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sidinei.valorantapp.ui.theme.ValorantAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainActivityContent()
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainActivityPreview() {
+    MainActivityContent()
+}
+
+@Composable
+fun MainActivityContent(){
+    ValorantAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Text(text = "MainActivity")
+        }
+    }
+}
